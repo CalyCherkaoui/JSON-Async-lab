e0134a4b06af41a89c28043be9bbaa30c184c8f2
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-12-29 12:08:40 -0600

    Fixing linters errors

--- src/index.js
+++ src/index.js
@@ -4,40 +4,40 @@ const header = document.querySelector('header');
 const section = document.querySelector('section');
 
 // Obtaining the JSON
-let requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
-let request = new XMLHttpRequest();
+const requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+const request = new XMLHttpRequest();
 
 // open request with GET
 request.open('GET', requestURL);
 
-//set the XHR request to convert the JSON response directly into a JavaScript object 
+// set the XHR request to convert the JSON response directly into a JavaScript object
 request.responseType = 'json';
 
-//Then we send the request with the send() method
+// Then we send the request with the send() method
 request.send();
 
 // involves waiting for the response to return from the server, then dealing with it.
-request.onload = function() {
+request.onload = () => {
   const superHeroes = request.response;
   populateHeader(superHeroes);
   showHeroes(superHeroes);
-}
+};
 
 // Populating the header
 function populateHeader(obj) {
   const myH1 = document.createElement('h1');
-  myH1.textContent = obj['squadName'];
+  myH1.textContent = obj.squadName;
 
   const myParagraph = document.createElement('p');
-  myParagraph.textContent = 'Hometown of the squad: ' + obj['homeTown'] + ' | Formed in : ' + obj['formed'] + ' | Secret Base location: ' + obj['secretBase'];
+  myParagraph.textContent = `Hometown of the squad: ${obj.homeTown} | Formed in : ${obj.formed} | Secret Base location: ${obj.secretBase}`;
   header.append(myH1, myParagraph);
 }
 
 // poulate the section with hero information cards
 function showHeroes(obj) {
-  const heroes = obj['members'];
+  const heroes = obj.members;
 
-  for (let i = 0; i < heroes.length; i++) {
+  for (let i = 0; i < heroes.length; i =+1) {
     const myArticle = document.createElement('article');
     const myH2 = document.createElement('h2');
     const myPara1 = document.createElement('p');
@@ -46,12 +46,12 @@ function showHeroes(obj) {
     const myList = document.createElement('ul');
 
     myH2.textContent = heroes[i].name;
-    myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
-    myPara2.textContent = 'Age: ' + heroes[i].age;
+    myPara1.textContent = `Secret identity: ${heroes[i].secretIdentity}`;
+    myPara2.textContent = `Age: ${heroes[i].age}`;
     myPara3.textContent = 'Superpowers:';
 
     const superPowers = heroes[i].powers;
-    for (let j = 0; j < superPowers.length; j++) {
+    for (let j = 0; j < superPowers.length; j =+1) {
       const listItem = document.createElement('li');
       listItem.textContent = superPowers[j];
       myList.appendChild(listItem);
@@ -67,46 +67,49 @@ function showHeroes(obj) {
   }
 }
 
-const happyFanForm = document.createElement('form');
-happyFanForm.setAttribute('id', 'happy_fan_form');
-const yesRadio = document.createElement('input');
-yesRadio.setAttribute('type', 'radio');
-yesRadio.setAttribute('name', 'happy?');
-yesRadio.setAttribute('value', 'happy Fan');
-const yesRadioLab = document.createElement('label');
-yesRadioLab.textContent = 'Yes! happy fan';
-
-const noRadio = document.createElement('input');
-noRadio.setAttribute('type', 'radio');
-noRadio.setAttribute('name', 'happy?');
-noRadio.setAttribute('value', 'unhappy fan');
-const noRadioLab = document.createElement('label');
-noRadioLab.textContent = 'No! unhappy fan';
-
-const submitBtn = document.createElement('button');
-submitBtn.setAttribute('id', 'happy_btn');
-submitBtn.textContent = 'Did the super hero squad get the fan vote?';
-
-const breakLn = document.createElement('br');
-
-happyFanForm.append(yesRadioLab, yesRadio, breakLn, noRadioLab, noRadio, submitBtn);
-
-section.appendChild(happyFanForm);
-
-const showFunVote = () => {
-  const radios = document.querySelectorAll('input[name="happy?"]');
-  for (const radio of radios) {
-    if (radio.checked) {
-      alert(radio.value);
-      break;
-      // return (radio.value === 'happy Fan');
-    }
-  }
-};
-
-submitBtn.addEventListener('click', showFunVote());
 
 
+// const happyFanForm = document.createElement('form');
+// happyFanForm.setAttribute('id', 'happy_fan_form');
+// const yesRadio = document.createElement('input');
+// yesRadio.setAttribute('type', 'radio');
+// yesRadio.setAttribute('name', 'happy?');
+// yesRadio.setAttribute('value', 'happy Fan');
+// const yesRadioLab = document.createElement('label');
+// yesRadioLab.textContent = 'Yes! happy fan';
 
+// const noRadio = document.createElement('input');
+// noRadio.setAttribute('type', 'radio');
+// noRadio.setAttribute('name', 'happy?');
+// noRadio.setAttribute('value', 'unhappy fan');
+// const noRadioLab = document.createElement('label');
+// noRadioLab.textContent = 'No! unhappy fan';
 
+// const submitBtn = document.createElement('button');
+// submitBtn.setAttribute('id', 'happy_btn');
+// submitBtn.textContent = 'Did the super hero squad get the fan vote?';
 
+// const breakLn = document.createElement('br');
+
+// happyFanForm.append(yesRadioLab, yesRadio, breakLn, noRadioLab, noRadio, submitBtn);
+
+// section.appendChild(happyFanForm);
+// let isHappyFan = false;
+
+// const showFunVote = () => {
+//   const radios = document.querySelectorAll('input[name="happy?"]');
+//   for (let i = 0; i < radios.length; i += 1) {
+//     if (radios[i].checked) {
+//       // alert(radio.value);
+//       // break;
+//       // isHappyFan = (radio.value === 'happy Fan');
+//       isHappyFan = radios[i].value;
+//       console.log(isHappyFan);
+
+//       break;
+//     }
+//   }
+// };
+
+// submitBtn.addEventListener('click', () => showFunVote());
+// // console.log(isHappyFan);
